0fcee9b8795689d08700b175d748afee08c3d6e5
jmindevelper <jmin.developer@gmail.com>
2022-09-03 20:52:16 +0900

    [feat] authError 정의 #6
    - 존재하는 이메일
    - 존재하지 않는 이메일
    - 비밀번호 불일치

--- BetweenUS/BetweenUS/Manager/AuthManager.swift
+++ BetweenUS/BetweenUS/Manager/AuthManager.swift
@@ -11,31 +11,71 @@ import FirebaseAuth
 
 final class AuthManager {
     
+    enum AuthError {
+        case error(type: String, message: String)
+    }
+    
     static let shared = AuthManager()
     
     private init() { }
     
     private let userDb = FBUserDatabase()
     
-    func signIn(email: String, password: String) {
+    func signIn(
+        email: String,
+        password: String,
+        completion: (() -> Void)? = nil,
+        errorCompletion: ((AuthError) -> Void)? = nil
+    ) {
         Auth.auth().signIn(
             withEmail: email,
             password: password
         ) { [weak self] result, error in
+            guard let self = self else { return }
+            if let error = error {
+                let errorCode = (error as NSError).code
+                guard let authError = self.branchError(code: errorCode) else { return }
+                errorCompletion?(authError)
+            }
             guard let result = result else { return }
-            self?.userDb.loadUser(id: result.user.uid) { user in
+            self.userDb.loadUser(id: result.user.uid) { user in
                 UserInformation.shared.user = user
             }
         }
     }
     
-    func signUp(email: String, password: String, user: User) {
+    func signUp(
+        email: String,
+        password: String,
+        user: User,
+        completion: (() -> Void)? = nil,
+        errorCompletion: ((AuthError) -> Void)? = nil
+    ) {
         logout()
         Auth.auth().createUser(withEmail: email, password: password) { [weak self] result, error in
             guard let self = self else { return }
+            if let error = error {
+                let errorCode = (error as NSError).code
+                print(errorCode, error.localizedDescription)
+                guard let authError = self.branchError(code: errorCode) else { return }
+                errorCompletion?(authError)
+            }
             guard let result = result else { return }
             
             self.userDb.saveNewUser(user: user, id: result.user.uid)
+            completion?()
+        }
+    }
+    
+    private func branchError(code: Int) -> AuthError? {
+        switch code {
+        case 17007:
+            return .error(type: "signUp", message: "이미 있는 이메일입니다")
+        case 17011:
+            return .error(type: "signIn", message: "존재하지 않는 이메일입니다")
+        case 17009:
+            return .error(type: "signIn", message: "비밀번호가 일치하지 않습니다")
+        default: return nil
         }
     }
     
