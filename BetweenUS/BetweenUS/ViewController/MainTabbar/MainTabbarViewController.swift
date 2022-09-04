2fa2fd36f64de34c52b3f0158fefa3194615d527
jmindevelper <jmin.developer@gmail.com>
2022-09-04 15:11:47 +0900

    [feat] 앱실행시 로그인상태이면 MainTabbarController가 RootVC #8

---
+++ BetweenUS/BetweenUS/ViewController/MainTabbar/MainTabbarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabbarViewController.swift
+//  BetweenUS
+//
+//  Created by J_Min on 2022/09/04.
+//
+
+import UIKit
+
+final class MainTabbarViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .viewBackground
+    }
+    
+}
